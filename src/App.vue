0d9a81586e8a76f8a15b0a88f72daaec7ebb9346
TreyAlexander <alexandertremayne1117@gmail.com>
2026-04-20 23:18:14 -0500

    Updates for simple navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Navbar />
+  <div id="app">
+    <Navbar :isAuthenticated="authenticated" @logout="logout" />
     <router-view />
   </div>
 </template>
@@ -12,6 +12,33 @@ export default {
   name: 'App',
   components: {
     Navbar
+  },
+  data() {
+    return {
+      authenticated: false
+    }
+  },
+  mounted() {
+    this.authenticated = localStorage.getItem('isAuthenticated') === 'true'
+  },
+  methods: {
+    setAuthState(value) {
+      this.authenticated = value
+      localStorage.setItem('isAuthenticated', String(value))
+    },
+    logout() {
+      localStorage.removeItem('log_user')
+      localStorage.removeItem('access')
+      localStorage.removeItem('refresh')
+      localStorage.setItem('isAuthenticated', 'false')
+      this.authenticated = false
+      this.$router.push('/login')
+    }
+  },
+  watch: {
+    $route() {
+      this.authenticated = localStorage.getItem('isAuthenticated') === 'true'
+    }
   }
 }
 </script>
